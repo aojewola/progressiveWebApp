4e06dfaa297b7bbc0b04ca2a486a002684a8cb87
Asimi <asimi.ojewola@interswitchgroup.com>
2021-10-13 10:57:23 +0100

    edadd network first strategies to the worker

--- service-worker.js
+++ service-worker.js
@@ -2,4 +2,4 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
-    new workbox.strategies.networkFirst());
+    new workbox.strategies.NetworkFirst());
